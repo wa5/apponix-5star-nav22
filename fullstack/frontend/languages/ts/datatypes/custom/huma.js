98d715598a81439f3b846df88a5845e7064229ce
wa5 <47422872+wa5@users.noreply.github.com>
2022-12-03 11:50:51 +0530

    jhh

---
+++ fullstack/frontend/languages/ts/datatypes/custom/huma.js
@@ -0,0 +1,22 @@
+console.log('hello im working');
+var eyes = 'ooo';
+var Human = /** @class */ (function () {
+    function Human() {
+        this.eyes = 'yes 2eyes';
+        this.brain = 'yes is threr';
+        this.heart = 'at home';
+        this.handes = 2;
+        this.leges = true;
+    }
+    Human.prototype.wt_uwatched_with_eyes_this_morning = function () {
+        return 'wt hell is wrong with u';
+    };
+    return Human;
+}());
+var youhan = new Human();
+console.log(youhan.brain);
+console.log(youhan.wt_uwatched_with_eyes_this_morning());
+var bottle = 'waterbottel';
+var bottle2 = new String('waterbottel');
+console.log(bottle);
+console.log(bottle2);
---
+++ fullstack/frontend/languages/ts/datatypes/custom/huma.ts
@@ -0,0 +1,29 @@
+console.log('hello im working')
+
+var eyes='ooo'
+class Human{
+eyes:string
+brain:string
+heart:string
+handes:number
+leges:boolean
+constructor(){
+    this.eyes='yes 2eyes'
+    this.brain='yes is threr'
+this.heart='at home'
+this.handes=2
+this.leges=true
+}
+wt_uwatched_with_eyes_this_morning(){
+    return 'wt hell is wrong with u'
+}
+}
+
+var youhan=new Human()
+console.log(youhan.brain)
+console.log(youhan.wt_uwatched_with_eyes_this_morning())
+
+var bottle='waterbottel'
+var bottle2=new String('waterbottel')
+console.log(bottle)
+console.log(bottle2)
