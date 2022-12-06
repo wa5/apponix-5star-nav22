0e3905f36a4245395f650dac88376a07ce8d8990
wa5 <47422872+wa5@users.noreply.github.com>
2022-12-06 11:31:50 +0530

    ff

---
+++ fullstack/frontend/languages/ts/functions/normal.ts
@@ -0,0 +1,25 @@
+function calling(){
+    console.log('its the magic dada')
+    console.log('its the magic dada')
+    console.log('its the magic dada')
+    console.log('its the magic dada')
+    console.log('its the magic dada')
+   
+}
+
+function calling1(myrapper:string){
+    console.log(myrapper)
+    console.log(myrapper)
+    console.log(myrapper)
+    console.log(myrapper)
+    console.log(myrapper)
+}
+calling1('hello')
+
+
+function birthdayofNaglashmi(a,b,c,d){
+    console.log(a,b,c,d)
+    return 'thank u'
+}
+
+console.log(birthdayofNaglashmi('watch','car','chocate','dress'))
