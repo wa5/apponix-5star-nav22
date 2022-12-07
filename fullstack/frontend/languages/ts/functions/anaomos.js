9a247cbba4d6bef2062391ec44daada5aebb661b
wa5 <47422872+wa5@users.noreply.github.com>
2022-12-07 11:32:51 +0530

    ss

--- fullstack/frontend/languages/ts/functions/anaomos.js
+++ fullstack/frontend/languages/ts/functions/anaomos.js
@@ -1,11 +1,7 @@
-
-
 // // var myfun = function () { console.log('testing im working') };
-
 // // console.log(myfun);
 // // myfun();
 // // (() => { console.log('testing im working fate arrow') })()
-
 // // var a = () => { }
 // // var j: string
 // // var n: number
@@ -13,27 +9,21 @@
 // // var mufun2: () => string
 // // mufun2 = () => { return 'shshhs' }
 // // (() => { return 'hi youhan' })()
-
-
 // var child = (a:number) => {console.log(a) }
-
 // var parent1 = (childerns: (a:number) => void) => { 
 //     console.log(childerns)
 //     var mynum=12345
 //     childerns(mynum)
 //     return childerns
 //  }
-
 // parent1(child)
-
-
-var j=()=>{
-    var myval=123445
-    var exp=()=>{
-        console.log(myval)
-    }
-    return exp
-}
-console.log(j())
-var myfun=j()
-myfun()
+var j = function () {
+    var myval = 123445;
+    var exp = function () {
+        console.log(myval);
+    };
+    return exp;
+};
+console.log(j());
+var myfun = j();
+myfun();
