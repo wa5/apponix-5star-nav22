5311710bb24df66457107865e92ea45dd7cf0b19
wa5 <47422872+wa5@users.noreply.github.com>
2022-12-20 11:23:51 +0530

    we have done rest

---
+++ fullstack/frontend/languages/ts/rest-spread-copyarry-copy-object/copyOfArry.js
@@ -0,0 +1,11 @@
+var myarry=[10,20,30]
+myarry.push(40)
+var copy=[...myarry]
+console.log(copy)
+console.log(myarry)
+
+console.log(copy)
+console.log("12",myarry)
+// copy.push(50)
+// console.log(copy)
+// console.log(myarry)
